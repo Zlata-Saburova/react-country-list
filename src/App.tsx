b5f4728b5993f56faf241338c9efb0e689387aad
Zlata <zlatasaburova97@gmail.com>
2022-04-11 14:23:51 +0300

    add types for countries

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import countryData from "./country-data.json";
+import { ICountry } from "./types";
 
 const App = () => {
-  return <div className="div"></div>;
+  const countries: ICountry[] = countryData.map((country) => ({
+    name: country.name.common,
+    capital: country.capital[0],
+    population: country.population,
+    flag: country.flags.svg,
+    area: country.area,
+    region: country.region,
+  }));
 };
 
 export default App;
---
+++ src/types/index.ts
@@ -0,0 +1,8 @@
+export interface ICountry {
+  name: string;
+  capital: string;
+  population: number;
+  flag: string;
+  area: number;
+  region: string;
+}
